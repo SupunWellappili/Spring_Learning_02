5bc0307a23a05a64a7e748ce43a60e94c3e80b06
SupunWellappili <supunchandana1814@gmail.com>
2024-06-28 18:46:51 +0530

    @Componet | @Bean --> Use Prototype

--- SpringBeanLifeCycle_02/src/AppInitializer.java
+++ SpringBeanLifeCycle_02/src/AppInitializer.java
@@ -1,3 +1,4 @@
+import bean.MyConnection;
 import bean.SpringBeanOne;
 import config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -10,12 +11,17 @@ public class AppInitializer {
         ctx.refresh();
         ctx.registerShutdownHook();
 
-        SpringBeanOne ref1 = ctx.getBean(SpringBeanOne.class);
+        /*SpringBeanOne ref1 = ctx.getBean(SpringBeanOne.class);
         SpringBeanOne ref2 = ctx.getBean(SpringBeanOne.class);
         SpringBeanOne ref3 = ctx.getBean(SpringBeanOne.class);
         System.out.println(ref1);
         System.out.println(ref2);
-        System.out.println(ref3);
+        System.out.println(ref3);*/
+
+        MyConnection conn1 = ctx.getBean(MyConnection.class);
+        MyConnection conn2 = ctx.getBean(MyConnection.class);
+        System.out.println(conn1);
+        System.out.println(conn2);
 
     }
 }
--- SpringBeanLifeCycle_02/src/bean/SpringBeanOne.java
+++ SpringBeanLifeCycle_02/src/bean/SpringBeanOne.java
@@ -1,11 +1,13 @@
 package bean;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
+//@Scope("prototype")
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class SpringBeanOne {
     public SpringBeanOne() {
         System.out.println("Spring Bean One");
