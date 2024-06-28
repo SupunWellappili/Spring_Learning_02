3662fede451c0e623904566c6e039c6cec279374
SupunWellappili <supunchandana1814@gmail.com>
2024-06-28 22:42:32 +0530

    Show Bean Lige Cycle Method

--- SpringBeanLifeCycle_02/src/bean/SpringBeanOne.java
+++ SpringBeanLifeCycle_02/src/bean/SpringBeanOne.java
@@ -1,21 +1,52 @@
 package bean;
 
-import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.naming.spi.InitialContextFactory;
+import javax.security.auth.Destroyable;
+
 @Component
-//@Scope("prototype")
-//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-public class SpringBeanOne implements DisposableBean {
+public class SpringBeanOne implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
+    //02
+    //private String id;
+
+    //01
     public SpringBeanOne() {
-        System.out.println("Spring Bean One");
+        System.out.println("Spring Bean One Initiantiated");
+    }
+
+    @Override
+    //03
+    public void setBeanName(String name) {
+        System.out.println("Bean name aware called");
+    }
+
+    @Override
+    //04
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Bean Factory aware called");
+    }
+
+    @Override
+    //05
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Application Context aware called");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Initializing Bean called");
     }
 
     @Override
     public void destroy() throws Exception {
-        System.out.println("Spring Bean one Destroy");
+        System.out.println("Disposable Bean called");
     }
 }
