1cf301f9189f2ea652e6b38334691a99c413a080
SupunWellappili <supunchandana1814@gmail.com>
2024-06-29 00:31:43 +0530

    Constructor Injection

--- DI_With_Spring_03/src/bean/Boy.java
+++ DI_With_Spring_03/src/bean/Boy.java
@@ -4,7 +4,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Boy {
-    Girl girl = new Girl();
+    //Girl girl = new Girl();//Property Injection
+
+
+    //Constructor Injection
+    Girl girl;
+    public Boy(Girl g) {
+        this.girl=g;
+    }
 
     public void chattingWithGirl() {
         girl.chat();
