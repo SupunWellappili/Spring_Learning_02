7da136800c95712a6c4ea6ad29b85d9c99d5bdb3
SupunWellappili <supunchandana1814@gmail.com>
2024-06-30 17:59:59 +0530

    Interface Injection with Spring

--- DI_With_Spring_03/src/di/Kamal.java
+++ DI_With_Spring_03/src/di/Kamal.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Kamal {
+public class Kamal implements DI {
 
     //@Autowired
     //GoodKamali  kamali; //Property Injection
@@ -16,13 +16,20 @@ public class Kamal {
         this.kamali = k;
     }*/
 
-    @Autowired
-    public void setKamali(GoodKamali k){
+   /* @Autowired
+    public void setKamali(GoodKamali k){ //Setter Method Injection
         System.out.println("Kamal is in the context");
         this.kamali=k;
-    }
+    }*/
 
     public void chattingWithKamali() {
         kamali.chat();
+    }
+
+    @Override
+    @Autowired
+    public void inject(GoodKamali kamali) {
+        System.out.println("Kamal is in the context");
+        this.kamali =kamali;
     }
 }
