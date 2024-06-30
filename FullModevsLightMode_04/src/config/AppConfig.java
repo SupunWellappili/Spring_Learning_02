2b9d7b9b64f9f304829a730b4ba618b5ee725490
SupunWellappili <supunchandana1814@gmail.com>
2024-07-01 00:16:09 +0530

    Inter bean Dependency - Singleon

--- FullModevsLightMode_04/src/config/AppConfig.java
+++ FullModevsLightMode_04/src/config/AppConfig.java
@@ -2,6 +2,7 @@ package config;
 
 
 import bean.SpringBeanOne;
+import bean.SpringBeanTwo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,8 +12,18 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     @Bean
-    public SpringBeanOne getting(){
+    public SpringBeanOne getBeanOne(){
+        //Inter Bean Dependency
+        SpringBeanTwo beanTwo1 = getBeanTwo();
+        SpringBeanTwo beanTwo2 = getBeanTwo();
+        System.out.println(beanTwo1);
+        System.out.println(beanTwo2);
         return new SpringBeanOne();
+    }
+
+    @Bean
+    public SpringBeanTwo getBeanTwo(){
+        return  new SpringBeanTwo();
     }
 
 }
