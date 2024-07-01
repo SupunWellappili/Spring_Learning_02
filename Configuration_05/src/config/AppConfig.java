89494d3d2ae24e2d52d7756431f0bf66423dadb6
SupunWellappili <supunchandana1814@gmail.com>
2024-07-01 19:21:56 +0530

    Runtime Value Injection Module Created

--- Configuration_05/src/config/AppConfig.java
+++ Configuration_05/src/config/AppConfig.java
@@ -9,8 +9,8 @@ import org.springframework.context.annotation.ImportResource;
 @Configuration
 @ComponentScan(basePackages = "bean")
 @Import({AppConfig01.class,AppConfig02.class})
-@ImportResource("classpath : abcd.xml") //Class path
-@ImportResource("file : absolute-path-of-abcd.xml") //Root Path (Machine / Server / Another PC)
+//@ImportResource("classpath : abcd.xml") //Class path
+//@ImportResource("file : absolute-path-of-abcd.xml") //Root Path (Machine / Server / Another PC)
 public class AppConfig {
 
 }
