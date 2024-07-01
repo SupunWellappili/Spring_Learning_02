c87033bf8ed9c7648b4f6e212ee876ac9eca9ec9
SupunWellappili <supunchandana1814@gmail.com>
2024-07-01 19:01:42 +0530

    @ImportResource - classpath/File

--- Configuration_05/src/config/AppConfig.java
+++ Configuration_05/src/config/AppConfig.java
@@ -1,12 +1,16 @@
 package config;
 
+import bean.SpringBeanFive;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ComponentScan(basePackages = "bean")
 @Import({AppConfig01.class,AppConfig02.class})
+@ImportResource("classpath : abcd.xml") //Class path
+@ImportResource("file : absolute-path-of-abcd.xml") //Root Path (Machine / Server / Another PC)
 public class AppConfig {
 
 }
