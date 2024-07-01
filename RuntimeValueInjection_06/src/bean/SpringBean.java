ea00ecd3604225c080ff506c5666336135180a8f
SupunWellappili <supunchandana1814@gmail.com>
2024-07-01 21:05:23 +0530

    Same para Metar Constructor Called (@Autowired+require+pere metar count)

--- RuntimeValueInjection_06/src/bean/SpringBean.java
+++ RuntimeValueInjection_06/src/bean/SpringBean.java
@@ -1,19 +1,23 @@
 package bean;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBean {
 
+    @Autowired(required = false)
     public SpringBean(@Value("C-001") String id, @Value("20") int age, @Value("true") boolean marred) {
         System.out.println(id);
         System.out.println(age);
         System.out.println(marred);
     }
 
-    @Autowired
+    @Autowired(required = false)
     public SpringBean(@Value("S-001,Supun,Galle,15000") String[] Details, @Value("A") char a) {
         for (String Detail : Details) {
             System.out.println(Detail);
