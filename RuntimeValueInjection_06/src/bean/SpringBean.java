1e2f604e8dd5f20526834d9d614c05d12c1f9fb5
SupunWellappili <supunchandana1814@gmail.com>
2024-07-01 20:50:20 +0530

    Same para Metar Constructor Called (@Autowired)

--- RuntimeValueInjection_06/src/bean/SpringBean.java
+++ RuntimeValueInjection_06/src/bean/SpringBean.java
@@ -7,10 +7,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class SpringBean {
 
-
-
-    public SpringBean(@Value("C001") String id) {
-        System.out.println("Spring Bean Instantiated");
+    public SpringBean(@Value("C-001") String id, @Value("20") int age, @Value("true") boolean marred) {
         System.out.println(id);
+        System.out.println(age);
+        System.out.println(marred);
+    }
+
+    @Autowired
+    public SpringBean(@Value("S-001,Supun,Galle,15000") String[] Details, @Value("A") char a) {
+        for (String Detail : Details) {
+            System.out.println(Detail);
+        }
+        System.out.println(a);
     }
 }
