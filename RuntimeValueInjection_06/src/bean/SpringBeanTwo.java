047f91f56016ce7d8a7f0ced05aafff221914d77
SupunWellappili <supunchandana1814@gmail.com>
2024-07-02 15:44:13 +0530

    System.getenv();

---
+++ RuntimeValueInjection_06/src/bean/SpringBeanTwo.java
@@ -0,0 +1,15 @@
+package bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBeanTwo {
+
+    //01.System Environment Variable
+    //02.Java Environment Variable
+    //03.Our software Environment Variable
+
+    public SpringBeanTwo() {
+
+    }
+}
