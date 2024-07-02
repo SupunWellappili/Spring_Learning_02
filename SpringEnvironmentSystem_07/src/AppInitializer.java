da189c2c14c953714d9d25fd82644b8d57ed8728
SupunWellappili <supunchandana1814@gmail.com>
2024-07-02 18:03:59 +0530

    Java Environment Variable

--- SpringEnvironmentSystem_07/src/AppInitializer.java
+++ SpringEnvironmentSystem_07/src/AppInitializer.java
@@ -2,6 +2,7 @@ import config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Map;
+import java.util.Properties;
 
 public class AppInitializer {
     public static void main(String[] args) {
@@ -11,9 +12,20 @@ public class AppInitializer {
         ctx.registerShutdownHook();
 
 
+        //System.getenv(); -> Access System Environment Variables with java
         Map<String, String> getenv = System.getenv();
         for ( String key:getenv.keySet()) {
             System.out.println(key+" : "+getenv.get(key));
+        }
+
+
+        System.out.println("-----------------------");
+
+
+        //System.getProperties(); --> Access Predefined properties from Java
+        Properties properties = System.getProperties();
+        for (Object key : properties.keySet()) {
+            System.out.println(key+" : "+properties.get(key));
         }
     }
 }
